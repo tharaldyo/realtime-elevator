1977285c69df7a75a315865b832d2dd78bee5b35
Student <student@itk-d650>
2017-02-17 16:43:10 +0100

    Created a network manager for testing purposes

---
+++ connection_manager.erl
@@ -0,0 +1,32 @@
+-module(connection_manager).
+-export([start/0]).
+
+-define(RECEIVE_PORT, 20066).
+-define(SEND_PORT, 20067).
+
+start() ->
+	spawn(fun -> listen/0),
+	spawn(fun -> broadcast/0).
+
+listen() ->
+	{ok, ReceiveSocket} = gen_udp:open(?RECEIVE_PORT, [list, {active, false}]),
+	listen(ReceiveSocket).
+
+listen(ReceiveSocket) ->
+	{ok, {_Address, _Port, NodeName}} = gen_udp:recv(ReceiveSocket, 0),
+	case list:member(NodeName, [node()|nodes()]) of
+		true ->
+			listen(ReceiveSocket)
+		false ->
+			net_adm:ping(list_to_atom(NodeName)) % ping node to create a connection
+			listen(ReceiveSocket)
+		end.
+
+broadcast() ->
+	{ok, SendSocket} = gen_udp:open(?SEND_PORT, [list, {active, true}, {broadcast, true}]),
+	broadcast(SendSocket).
+
+broadcast(SendSocket) ->
+	ok = gen_udp:send(SendSocket, {255,255,255,255}, ?RECEIVE_PORT, atom_to_list(node())),
+	timer:sleep(2000),
+	broadcast(SendSocket).
--- elevator.erl
+++ elevator.erl
@@ -2,20 +2,21 @@
 -export([start/0]).
 
 start() ->
+	connection_manager:start(),
 	IdlePID = spawn(fun idle/0),
 	elev_driver:start(IdlePID, elevator),
 	io:format("Elevator pid: ~p~n", [self()]),
-	io:format("hello from below spawn").
+	io:format("hello from below spawn~n"). %debug
 
 
 idle() ->
 	receive
 		{floor_reached, 0} ->
 			elev_driver:set_motor_direction(up),
-			io:format("floor reached: 0 ~n");
+			io:format("floor reached: 0 ~n"); %debug
 		{floor_reached, 2} ->
 			elev_driver:set_motor_direction(down),
-			io:format("floor reached: 2 ~n");
+			io:format("floor reached: 2 ~n"); %debug
 		_Message ->
 			io:format("~p~n", [_Message])
 
