f2db1984ddf78b5cc82480593a079d57e032a66b
Kristian Fjelde Pedersen <johan.korsnes@gmail.com>
2017-03-02 14:19:23 +0100

    Idle loop in order_manager.

--- compile.sh
+++ compile.sh
@@ -1,6 +1,7 @@
 erlc *.erl;
-mkdir ebin/;
+mkdir ebin/ 2> /dev/null;
 mv *.beam ebin/;
 make -C driver /;
 cd ebin/;
 erl
+cd ..;
--- order_manager.erl
+++ order_manager.erl
@@ -1,39 +1,22 @@
 -module(order_manager).
--export([start/0, temp_recv/0]).
--record (order, {floor, direction}). % is this right?
+-export([start/0]).
+-record (order, {floor, direction}).
 
 start() ->
 	register(orderman, self()),
-	io:format("Orderman online and ready~n"),
-	ok = mnesia:create_schema([node()]), % create schema on this node only, use [node()|nodes()] to install on all nodes
-	application:start(mnesia),
-	mnesia:create_table(order, [{attributes, record_info(fields, order)},
-															{disc_copies, [node()]}]),
-	io:format("Mnesia initialized~n"),
-	%#order{floor=1, direction=up},
-	Test = #order{floor=2, direction=down},
+	io:format("order_manager: registered, now spawning loop.~n"),
+    spawn(fun loop/0).
 
-	% Mnesia stuff largely incomplete
-
-	NewOrderDirection=Test#order.direction,
-	io:format("NewOrderDirection: ~p~n", [NewOrderDirection]).
-
-
-
-
-temp_recv() ->
-
-	receive
-		{floor_reached, 0} ->
-			elev_driver:set_motor_direction(up),
-			io:format("~p~n", [{floor_reached, 0}]); %debug
-		{floor_reached, 2} ->
-			elev_driver:set_motor_direction(down),
-			io:format("~p~n", [{floor_reached, 2}]); %debug
-		_Message ->
-			io:format("~p~n", [_Message])
-
-		end,
-
-
-	temp_recv().
+loop() ->
+  receive
+    {new_order, Direction, Floor} ->
+      % distribute new order
+      io:format("order_manager: new order: ~p~p~n", [Direction, Floor]);
+    {floor_reached, Floor} ->
+      % update FSM?
+      io:format("order_manager: floor reached: ~p~n", [Floor]);
+    _Message ->
+      io:format("order_manager: received invalid message: ~p~n",
+                [_Message])
+  end,
+  loop().
